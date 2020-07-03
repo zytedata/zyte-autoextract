af4754cf1f032083bee47f53322cb791683c82b2
Victor Torres <vpaivatorres@gmail.com>
2020-07-03 18:16:06 -0300

    Refactor tests
    QueryError was updated with the addition of the DomainOccupied class

--- tests/test_aio/test_errors.py
+++ tests/test_aio/test_errors.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from contextlib import suppress
-
 import pytest
 
 from autoextract.aio.errors import QueryError
 
 
-def get_error_response(message):
+def get_query_error(message):
     return {
         "query": {
             "id": "1587642195276-9386233af6ce1b9f",
@@ -22,32 +20,37 @@ def get_error_response(message):
 
 def test_query_error():
     message = "Downloader error: http404"
-    response = get_error_response(message)
+    response = get_query_error(message)
     exc = QueryError(response["query"], response["error"])
     assert exc.query == response["query"]
     assert exc.message == response["error"]
+    assert exc.retriable is False
+    assert exc.domain_occupied is None
 
 
-@pytest.mark.parametrize("message, domain_occupied, retry_seconds", (
+@pytest.mark.parametrize("message, domain, retry_seconds", (
     ("Domain example.com is occupied, please retry in 23.5 seconds", "example.com", 23.5),
     ("domain example.net is occupied, please retry in 23.5 seconds", "example.net", 23.5),
     ("Domain example.com is occupied, please retry in 23 seconds", "example.com", 23),
     ("domain example.net is occupied, please retry in 23 seconds", "example.net", 23),
-    ("Domain example.com is occupied, please retry in asd seconds", "example.com", ValueError),
-    ("domain example.net is occupied, please retry in xyz seconds", "example.net", ValueError),
-    ("domain example.net is occupied, please retry in  seconds", None, None),
-    ("domain example.net is occupied, please retry in seconds", None, None),
-    ("foo bar", None, None),
+    ("Domain example.com is occupied, please retry in asd seconds", "example.com", 300),  # 5 minutes default
+    ("domain example.net is occupied, please retry in xyz seconds", "example.net", 300),  # 5 minutes default
 ))
-def test_domain_occupied_query_error(message, domain_occupied, retry_seconds):
-    response = get_error_response(message)
-    exc = QueryError(response["query"], response["error"])
-    assert exc.query == response["query"]
-    assert exc.message == response["error"]
-    assert exc.domain_occupied == domain_occupied
+def test_domain_occupied_query_error(message, domain, retry_seconds):
+    query_error = get_query_error(message)
+    exc = QueryError.from_query_result(query_error)
+    assert exc.domain_occupied.domain == domain
+    assert exc.domain_occupied.retry_seconds == retry_seconds
+    assert exc.retry_seconds == retry_seconds
 
-    if isinstance(retry_seconds, type) and issubclass(retry_seconds, Exception):
-        with pytest.raises(retry_seconds):
-            exc.retry_seconds
-    else:
-        assert exc.retry_seconds == retry_seconds
+
+@pytest.mark.parametrize("message", (
+    "domain example.net is occupied, please retry in  seconds",
+    "domain example.net is occupied, please retry in seconds",
+    "foo bar",
+))
+def test_invalid_domain_occupied_query_error(message):
+    query_error = get_query_error(message)
+    exc = QueryError.from_query_result(query_error)
+    assert exc.domain_occupied is None
+    assert exc.retry_seconds is None

f1e6b09657cd2ff0b343152de973d6e44f40293d
Victor Torres <vpaivatorres@gmail.com>
2020-07-06 20:10:07 -0300

    Add test case to make sure not retriable errors are not raised

--- tests/test_aio/test_request_processor.py
+++ tests/test_aio/test_request_processor.py
@@ -71,6 +71,74 @@ def test_request_processor_without_retries():
     assert request_processor.pending_queries == []
 
 
+def test_request_processor_with_not_retriable_error():
+    # Given an initial query with two items
+    initial_query = [
+        {
+            "url": "https://example.org/first",
+            "pageType": "article",
+        },
+        {
+            "url": "https://example.org/second",
+            "pageType": "article",
+        },
+    ]
+
+    # Initialize our request processor with this query
+    request_processor = RequestProcessor(
+        query=initial_query,
+        handle_retries=True
+    )
+
+    # Since we haven't processed any response yet,
+    # our request processor should have:
+    #
+    # Empty results
+    assert request_processor.get_latest_results() == []
+    # Pending queries list equal to our initial query
+    assert request_processor.pending_queries == initial_query
+
+    # Given our first response with a success and an error
+    first_response = [
+        {
+            "query": {
+                "id": "1",
+                "domain": "example.org",
+                "userQuery": {
+                    "url": "https://example.org/first",
+                    "pageType": "article"
+                }
+            },
+            "article": {
+                "name": "My first article",
+            },
+        },
+        {
+            "query": {
+                "id": "2",
+                "domain": "example.org",
+                "userQuery": {
+                    "url": "https://example.org/second",
+                    "pageType": "article"
+                }
+            },
+            "error": "Downloader error: http404",
+        },
+    ]
+
+    # When processing this response, no QueryError should be raised
+    # because the 404 error is not retriable
+    results = request_processor.process_results(first_response)
+
+    # Results should be equal to our response
+    assert results == first_response
+    # Latest results should be equal to our response
+    assert request_processor.get_latest_results() == results
+    # Our pending queries list should be empty
+    # because the errors found are not retriable
+    assert request_processor.pending_queries == []
+
+
 def test_request_processor_with_retries():
     # Given an initial query with two items
     initial_query = [

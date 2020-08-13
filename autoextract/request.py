53692d5aaa330e3df692ae21932e7ab7ae5b5721
Victor Torres <vpaivatorres@gmail.com>
2020-08-13 10:06:56 -0300

    Add support for extra API params and change fullHtmml default to False

--- autoextract/request.py
+++ autoextract/request.py
@@ -12,9 +12,13 @@ class Request:
     A single request data for AutoExtract.
     See https://doc.scrapinghub.com/autoextract.html#requests
 
-    Note that articleBodyRaw is set to false by default here; API itself
-    defaults to true. Set articleBodyRaw=None to remove articleBodyRaw
+    Note that `articleBodyRaw` is set to false by default here; API itself
+    defaults to true. Set `articleBodyRaw=None` to remove `articleBodyRaw`
     parameter from the request and use server default.
+
+    You can override API params passing a dictionary with extra data using the
+    `extra` argument. Note that it will overwrite any previous configuration
+    made using standard attributes like `articleBodyRaw` and `fullHtml`.
     """
     url = attr.ib()  # type: str
     pageType = attr.ib()  # type: str
@@ -23,8 +27,12 @@ class Request:
     articleBodyRaw = attr.ib(default=False)  # type: Optional[bool]
     fullHtml = attr.ib(default=None)  # type: Optional[bool]
 
+    extra = attr.ib(default=None)  # type: Optional[dict]
+
     def as_dict(self) -> QueryDict:
         d = attr.asdict(self)
+        d.update(**self.extra or {})
+        del d['extra']
         return {key: value for key, value in d.items() if value is not None}
 
 

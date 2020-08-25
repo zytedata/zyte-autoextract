aca36cf323645b39954d2bd8eabf5801c5d42df3
Victor Torres <vpaivatorres@gmail.com>
2020-08-25 11:39:14 -0300

    Improve get_apikey method and its tests

--- tests/test_apikey.py
+++ tests/test_apikey.py
@@ -9,6 +9,6 @@ def test_get_apikey(autoextract_env_variable):
     assert get_apikey() == autoextract_env_variable
 
 
-def test_get_apikey_missing():
+def test_get_apikey_missing(autoextract_blank_env_variable):
     with pytest.raises(NoApiKey):
         get_apikey()
